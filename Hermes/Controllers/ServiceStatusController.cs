94542eb2bea903ffd68ba03ce24399f079d587a0
William Silva <william.silva@viannasempre.com.br>
2025-03-30 21:29:01 -0300

    Criação do controlador ServiceStatus para verificar o status do serviço

---
+++ Hermes/Controllers/ServiceStatusController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hermes.Controllers
+{
+    [Route("api/v1/healthz")]
+    [ApiController]
+    public class ServiceStatusController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult Get()
+        {
+            var healthCheck = new
+            {
+                status = "Serviço executando..."
+            };
+
+            return Ok(healthCheck);
+        }
+    }
+}
