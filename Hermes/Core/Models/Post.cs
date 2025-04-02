334dc99063faa45e3f24515c486e211ca9bf7909
William Silva <william.silva@viannasempre.com.br>
2025-04-02 10:56:11 -0300

    Adição de mais detalhamento nos models User e Post, para melhorar o mapeamento das propriedades com as tabelas.

--- Hermes/Core/Models/Post.cs
+++ Hermes/Core/Models/Post.cs
@@ -1,13 +1,28 @@
-﻿namespace Hermes.Core.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Hermes.Core.Models
 {
     public class Post
     {
+        [Column("id")]
         public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Column("title")]
         public string Title { get; set; } = string.Empty;
+
+        [Column("content")]
         public string Content { get; set; } = string.Empty;
+
+        [Column("author_id")]
         public Guid AuthorId { get; set; } = Guid.NewGuid();
+
+        [Column("created_at")]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public DateTime? UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        [Column("updated_at")]
+        public DateTime? UpdatedAt { get; set; }
+
+        [Column("is_published")]
         public bool IsPublished { get; set; } = true;
     }
 }
--- Hermes/Core/Models/User.cs
+++ Hermes/Core/Models/User.cs
@@ -1,16 +1,32 @@
-﻿using Hermes.Core.Enums;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Hermes.Core.Enums;
 
 namespace Hermes.Core.Models
 {
     public class User
     {
+        [Column("id")]
         public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Column("name")]
         public string Name { get; set; } = string.Empty;
+
+        [Column("email")]
         public string Email { get; set; } = string.Empty;
+
+        [Column("password")]
         public string Password { get; private set; }
+
+        [Column("role")]
         public Role Role { get; set; } = Role.User;
+
+        [Column("created_at")]
         public DateTime? CreatedAt { get; set; } = DateTime.UtcNow;
-        public DateTime? UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        [Column("updated_at")]
+        public DateTime? UpdatedAt { get; set; }
+
+        [Column("is_active")]
         public bool IsActive { get; set; } = true;
 
         public void SetPassword(string password)
