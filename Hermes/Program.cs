c78b6068d02263cc8fb55cf5d14eb71180720f34
William Bruno <william.silva@viannasempre.com>
2025-06-26 10:21:08 -0300

    Remoção do acesso ao Swagger em ambiente de produção Adição de redirecionamento HTTPS em pordução (UseHttpsRedirection)

--- Hermes/Program.cs
+++ Hermes/Program.cs
@@ -46,7 +46,7 @@ namespace Hermes
 
             // Database Connection.
             builder.Services.AddSingleton<IDbConnectionFactory>(provider =>
-            new PostgresConnectionFactory(connectionString));
+                new PostgresConnectionFactory(connectionString));
             SqlMapper.AddTypeHandler(new RoleTypeHandler());
             SqlMapper.AddTypeHandler(new DateOnlyTypeHandler());
 
@@ -60,16 +60,14 @@ namespace Hermes
             builder.Services.AddScoped<IVisitorService, VisitorService>();
 
             var app = builder.Build();
+            
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
-            // Configure the HTTP request pipeline.
-            //if (app.Environment.IsDevelopment())
-            //{
-            app.UseSwagger();
-            app.UseSwaggerUI();
-            //}
-
-            //app.UseHttpsRedirection();
-
+            app.UseHttpsRedirection();
             app.UseCors("AllowSpecificOrigins");
             app.UseAuthentication();
             app.UseAuthorization();
