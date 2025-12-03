695d326619cddf83d885c8dea51cd8c27867e346
William Bruno <william.silva@viannasempre.com.br>
2025-12-03 20:30:55 -0300

    Enable Swagger in dev and enforce HTTPS redirection
    The HTTP request pipeline in `Program.cs` was updated:
    - Swagger middleware (`app.UseSwagger()` and `app.UseSwaggerUI()`) is now conditionally enabled only in the development environment.
    - Uncommented `app.UseHttpsRedirection()` to enforce HTTPS redirection.
    These changes improve development experience and ensure secure communication.

--- Hermes/Program.cs
+++ Hermes/Program.cs
@@ -59,13 +59,13 @@ namespace Hermes
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            //if (app.Environment.IsDevelopment())
-            //{
-            app.UseSwagger();
-            app.UseSwaggerUI();
-            //}
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
             app.UseCors("AllowSpecificOrigins");
             app.UseAuthentication();
